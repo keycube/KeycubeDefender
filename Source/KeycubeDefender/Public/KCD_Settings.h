9c5a4e5ff1378548e16f4e26bd5d57b12db7bbe0
Falerhon <alex-lex360@hotmail.com>
2024-06-13 16:47:37 -0400

    Fix : Audio now saves between game instances

---
+++ Source/KeycubeDefender/Public/KCD_Settings.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameUserSettings.h"
+#include "KCD_Settings.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KEYCUBEDEFENDER_API UKCD_Settings : public UGameUserSettings
+{
+	GENERATED_BODY()
+	
+};
